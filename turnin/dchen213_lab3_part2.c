664e21521748855687e9c22bcb358a0701ad5b88
daisydsc <61919281+daisydsc@users.noreply.github.com>
2021-04-11 21:53:21 -0700

    Complete part 2

--- turnin/dchen213_lab3_part2.c
+++ turnin/dchen213_lab3_part2.c
@@ -15,37 +15,37 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
-	
+
 	unsigned char tempA = 0x00;
-	unsigned char tempC = 0x00;
-	unsigned char level = 0x00;
+	unsigned char tempC = 0x00; 
+  
+    	while (1) {
+		tempC = 0x00;
+		tempA = PINA & 0x0F;	
 
-    while (1) {
-	tempA = PINA;
-	level = 0x00;
-
-	level = tempA & 0x0F;
-
-	switch(level){
-		case 0x00: tempC = 0x40; break;
-		case 0x01:
-                case 0x02: tempC = 0x60; break;
-                case 0x03:
-                case 0x04: tempC = 0x70; break;
-                case 0x05:
-                case 0x06: tempC = 0x38; break;
-                case 0x07:
-                case 0x08:
-                case 0x09: tempC = 0x3C; break;
-                case 0x0A:
-                case 0x0B:
-                case 0x0C: tempC = 0x3E; break;
-                case 0x0D:
-                case 0x0E:
-                case 0x0F: tempC = 0x3F; break;
-                default: tempC = 0x00; break;
+	if (tempA >= 13) {
+		tempC = 0x3F;
+	}
+	else if (tempA >= 10) {
+                tempC = 0x3E;
         }
+	else if (tempA >= 7) {
+                tempC = 0x3C;
+        }
+	else if (tempA >= 5) {
+                tempC = 0x38;
+        }
+	else if (tempA >= 3) {
+                tempC = 0x70;
+        }
+	else if (tempA >= 1) {
+                tempC = 0x60;
+        }
+	else {
+		tempC = 0x40;
+	}
+
 	PORTC = tempC;
     }
-    return 1;
+return 1;
 }
